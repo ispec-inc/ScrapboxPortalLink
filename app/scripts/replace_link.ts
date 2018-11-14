5534f38ced846b9e8684909764f167f5a8881080
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2018-11-14 17:44:09 +0900

    Update 他プロジェクトにリンクが存在したら返す

--- app/scripts/replace_link.ts
+++ app/scripts/replace_link.ts
@@ -1,13 +1,16 @@
 import * as $ from 'jquery';
 import { ScrapboxRequest } from './request/scrapbox_request';
 
-console.log('hellooooooo');
 const scbRequest = new ScrapboxRequest();
 
 $(window).keydown(function(e) {
   const emptyPageElements = findEmptyPageLink();
+
+  emptyPageElements.forEach(element => {
+    console.log(element.innerText);
+  });
 });
 
-function findEmptyPageLink(): object[] {
+function findEmptyPageLink(): HTMLElement[] {
   return $('.empty-page-link').get();
 }
--- app/scripts/request/scrapbox_request.ts
+++ app/scripts/request/scrapbox_request.ts
@@ -4,11 +4,23 @@ import {PageList} from '../model/page_list';
 
 export class ScrapboxRequest {
 
+  static scrapboxBaseUrl: string = '';
+
+  projectName: string;
   pageList: PageList = new PageList();
 
   constructor() {
-    $.getJSON('https://scrapbox.io/api/pages/help-jp', function (json) {
+    this.projectName = 'help-jp';
+    $.getJSON(`https://scrapbox.io/api/pages/${this.projectName}`, function (json) {
       this.pageList = $.extend(new PageList(), json);
     });
+  }
+
+  public pageUrl(title: string): string | null {
+    if (this.pageList.hasPage(title)) {
+      return this.projectName + title;
+    }
+
+    return null;
   }
 }
