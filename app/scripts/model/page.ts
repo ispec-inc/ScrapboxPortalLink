005f40bd4f065e1d54e9f08a2cb219e49d654fb7
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2018-11-23 21:23:10 +0900

    Update Model page

--- app/scripts/model/page.ts
+++ app/scripts/model/page.ts
@@ -14,14 +14,14 @@ export class Page {
   accessed: number;
 
   constructor(data: any) {
-    this.id = 'id' in data ? data.id : 0;
-    this.title = 'title' in data ? data.title : 0;
-    this.image = 'image' in data ? data.image : 0;
-    this.user = 'user' in data ? data.user : 0;
+    this.id = 'id' in data ? data.id : '';
+    this.title = 'title' in data ? data.title : '';
+    this.image = 'image' in data ? data.image : '';
+    this.user = 'user' in data ? data.user : '';
     this.pin = 'pin' in data ? data.pin : 0;
     this.views = 'views' in data ? data.views : 0;
     this.linked = 'linked' in data ? data.linked : 0;
-    this.commitId = 'commitId' in data ? data.commitId : 0;
+    this.commitId = 'commitId' in data ? data.commitId : '';
     this.created = 'created' in data ? data.created : 0;
     this.updated = 'updated' in data ? data.updated : 0;
     this.accessed = 'accessed' in data ? data.accessed : 0;
